47e120fe48418b2e6db378e1c16a6287654b47d9
Pedro Ojeda <pedro_ojedafp@hotmail.es>
2018-08-22 11:13:04 -0500

    first commit

---
+++ sw.js
@@ -0,0 +1,78 @@
+//Asignar nombre y version de la cahce
+const CACHE_NAME ='v1_cache_victor_robles_pwa';
+
+//Ficheros a cachear en la aplicacion
+var urlsToCache = [
+	'./',
+	'./css/styles.css',
+	'./img/favicon.png',
+	'./img/1.png',
+	'./img/2.png',
+	'./img/3.png',
+	'./img/5.png',
+	'./img/6.png',
+	'./img/facebook.png',
+	'./img/instagram.png',
+	'./img/twitter.png',
+	'./img/favicon-1024.png',
+	'./img/favicon-512.png',
+	'./img/favicon-384.png',
+	'./img/favicon-256.png',
+	'./img/favicon-192.png',
+	'./img/favicon-128.png',
+	'./img/favicon-96.png',
+	'./img/favicon-64.png',
+	'./img/favicon-32.png',
+	'./img/favicon-16.png'
+	
+];
+//evento install
+//instalacopm del serviceworker y guardar en cache los recursos estaticos
+self.addEventListener('install',e=>{
+	e.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(cache =>{
+			return cache.addAll(urlsToCache)
+						.then(() =>{
+							self.skipWaiting();
+						})
+						.catch(err => {
+							console.log('No se ha registrado el cache',err);
+						})
+			})
+	);
+});
+
+//Evento activate
+self.addEventListener('activate',e =>{
+	const cacheWhitelist=[CACHE_NAME];
+	e.waitUntil(
+		caches.keys()
+		.then(cacheNames => {
+			return Promise.all( 
+				cacheNames.map(cacheName =>{
+					if(cacheWhitelist.indexOf(cacheName) === - 1){
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+		.then(() => {
+			self.clients.claim();
+		})
+	);
+});
+									
+//Evento fetch
+self.addEventListener('fetch',e=>{
+	e.respondWith(
+	caches.match(e.request)
+		  .then(res => {
+			  if(res){
+				  //devuelvo datos desde cache
+				  return res;
+			  }
+			  return fetch(e.request);
+		  })
+	);
+});
